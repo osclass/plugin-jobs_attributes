f8dd605ad3fd9c90cd830b6275ef6d9ec8d7aa53
AlaskanDesigns <holly@alaskandesigns.com>
2013-11-06 13:17:32 -0900

    bender styles
    converted to bender style

--- jobs_attributes/item_detail.php
+++ jobs_attributes/item_detail.php
@@ -7,65 +7,65 @@
 ?>
 <h2><?php _e('Job details', 'jobs_attributes'); ?></h2>
 <div class="job-detail">
-    <table>
+    
         <?php if(@$relations[$index] != "") { ?>
-        <tr>
-            <td><label for="relation"><?php _e('Relation', 'jobs_attributes'); ?></label></td>
-            <td><?php echo @$relations[$index]; ?></td>
-        </tr>
+        <div class="control-group">
+            <label class="control-label" for="relation"><?php _e('Relation', 'jobs_attributes'); ?></label>
+            <p><?php echo @$relations[$index]; ?><p>
+        </div>
         <?php } ?>
         <?php if(@$detail['s_company_name'] != "") { ?>
-        <tr>
-            <td><label for="companyName"><?php _e('Company name', 'jobs_attributes'); ?></label></td>
-            <td><?php echo @$detail['s_company_name']; ?></td>
-        </tr>
+        <div class="control-group">
+            <label class="control-label" for="companyName"><?php _e('Company name', 'jobs_attributes'); ?></label>
+            <p><?php echo @$detail['s_company_name']; ?><p>
+        </div>
         <?php } ?>
         <?php if(@$detail['e_position_type'] != "") { ?>
-        <tr>
-            <td><label for="positionType"><?php _e('Position type', 'jobs_attributes'); ?></label></td>
-            <td><?php echo $positions[$detail['e_position_type']]; ?></td>
-        </tr>
+        <div class="control-group">
+            <label class="control-label" for="positionType"><?php _e('Position type', 'jobs_attributes'); ?></label>
+            <p><?php echo $positions[$detail['e_position_type']]; ?><p>
+        </div>
         <?php } ?>
         <?php if(@$detail['s_salary_text'] != "" ) { ?>
-        <tr>
-            <td><label for="salaryText"><?php _e('Salary', 'jobs_attributes'); ?></label></td>
-            <td><?php echo @$detail['s_salary_text']; ?></td>
-        </tr>
+        <div class="control-group">
+            <label class="control-label" for="salaryText"><?php _e('Salary', 'jobs_attributes'); ?></label>
+            <p><?php echo @$detail['s_salary_text']; ?></p>
+        </div>
         <?php } ?>
-    </table>
+   
     <?php if(isset($detail['locale'][$locale]['s_desired_exp']) && $detail['locale'][$locale]['s_desired_exp']!='') { ?>
     <div>
-        <label for="desired_exp"><?php _e('Desired experience', 'jobs_attributes'); ?></label>
+        <label class="control-label" for="desired_exp"><?php _e('Desired experience', 'jobs_attributes'); ?></label>
         <p><?php echo @$detail['locale'][$locale]['s_desired_exp']; ?></p>
     </div>
     <?php } ?>
     <?php if(isset($detail['locale'][$locale]['s_studies']) && $detail['locale'][$locale]['s_studies']!='') { ?>
     <div>
-        <label for="studies"><?php _e('Studies', 'jobs_attributes'); ?></label>
+        <label class="control-label" for="studies"><?php _e('Studies', 'jobs_attributes'); ?></label>
         <p><?php echo @$detail['locale'][$locale]['s_studies']; ?></p>
     </div>
     <?php } ?>
     <?php if(isset($detail['locale'][$locale]['s_minimum_requirements']) && $detail['locale'][$locale]['s_minimum_requirements']!='') { ?>
     <div>
-        <label for="min_reqs"><?php _e('Minimum requirements', 'jobs_attributes'); ?></label>
+        <label class="control-label" for="min_reqs"><?php _e('Minimum requirements', 'jobs_attributes'); ?></label>
         <p><?php echo nl2br( @$detail['locale'][$locale]['s_minimum_requirements'] ) ; ?></p>
     </div>
     <?php } ?>
     <?php if(isset($detail['locale'][$locale]['s_desired_requirements']) && $detail['locale'][$locale]['s_desired_requirements']!='') { ?>
     <div>
-        <label for="desired_reqs"><?php _e('Desired requirements', 'jobs_attributes'); ?></label>
+        <label class="control-label" for="desired_reqs"><?php _e('Desired requirements', 'jobs_attributes'); ?></label>
         <p><?php echo nl2br( @$detail['locale'][$locale]['s_desired_requirements'] ); ?></p>
     </div>
     <?php } ?>
     <?php if(isset($detail['locale'][$locale]['s_contract']) && $detail['locale'][$locale]['s_contract']!='') { ?>
     <div>
-        <label for="contract"><?php _e('Contract', 'jobs_attributes'); ?></label>
+        <label class="control-label" for="contract"><?php _e('Contract', 'jobs_attributes'); ?></label>
         <p><?php echo @$detail['locale'][$locale]['s_contract']; ?></p>
     </div>
     <?php } ?>
     <?php if(isset($detail['locale'][$locale]['s_company_description']) && $detail['locale'][$locale]['s_company_description']!='') { ?>
     <div>
-        <label for="company_desc"><?php _e('Company description', 'jobs_attributes'); ?></label>
+        <label class="control-label" for="company_desc"><?php _e('Company description', 'jobs_attributes'); ?></label>
         <p><?php echo nl2br( @$detail['locale'][$locale]['s_company_description'] ) ; ?></p>
     </div>
     <?php } ?>
